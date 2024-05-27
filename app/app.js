7b2db1a31b0a2bf190eaa6d064fd0c1360eb3a1f
pedwoo <pedale.velocedp@gmail.com>
2024-05-27 14:34:23 +0200

    Added vue files and reorganized project

--- app/app.js
+++ app/app.js
@@ -1,47 +1,7 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const User = require("./models/user");
-const dotenv = require('dotenv');
+const express = require('express')
+const app = express()
+const registration = require('./register.js')
 
-const router = express.Router();
-router.use(express.json());
-dotenv.config();
-
-// connecting to database (not necessary since unified)
-// mongoose.connect(process.env.DATABASE_URI)
-// .then(()=> console.log("Connected to MongoDB"))
-// .catch(err => console.error("Could not connect to MongoDB"));
-
-router.post('/register', async (req, res) => {
-    try {
-        const { userType, email, FirstName, LastName, username, gender, password, birthDay } = req.body;
-
-        // Checking if user already exists in the database
-        const existingUser = await User.findOne({ email: email });
-        if (existingUser) {
-            return res.status(409).json({ message: "User already exists" });
-        }
-
-        const newUser = new User({
-            userType,
-            email,
-            FirstName,
-            LastName,
-            username,
-            gender,
-            password,
-            birthDay
-        });
-
-        await newUser.save();
-        res.status(201).json({ message: "User created successfully" });
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-});
-
-const app = express();
-app.use('/', router);
-// app.listen(3000);
+app.use(express.json())
+app.use('/register', registration)
+module.exports = app
---
+++ src/views/AboutView.vue
@@ -0,0 +1,17 @@
+<!-- views/About.vue -->
+<template>
+    <div>
+      <h1>About Page</h1>
+      <router-link to="/" class="text-body" role="menuitem" aria-label="Home">Home</router-link>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    // Component logic
+  };
+  </script>
+  
+  <style>
+  /* Component-specific styles */
+  </style>
