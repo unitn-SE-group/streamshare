cce07f2ed30ff0a71d8c9853b61fd333175f828c
Davide <155072781+IGINOI@users.noreply.github.com>
2024-06-17 00:02:39 +0200

    The documentation is now up to fate

--- app/authentication.js
+++ app/authentication.js
@@ -14,7 +14,7 @@ dotenv.config();
  */
 
 /**
- * POST /auth/login
+ * auth/login
  * 
  * @description This endpoint authenticates a user and returns an Access Token that can be used for subsequent API requests and a Refresh Token.
  * 
@@ -22,56 +22,71 @@ dotenv.config();
  * 
  * @endpoint_url https://api.yourservice.com/auth/login
  * 
- * @request_header 
  * 
- * @request_body The request body should be a JSON object containing the user's credentials: email and password.
  * 
- * @param {string} email The email of the user 
+ * @request
+ *      @Request_Header 
  * 
- * @param {string} password The password of the user
+ *      @Request_Body The request body should be a JSON object containing the user's credentials: email and password.
+ *          @param {string} email The email of the user 
+ *          @param {string} password The password of the user
  * 
- * @example {json} Example Request:
- *    {
- *      "email": "user@example.com",
- *      "password": "yourpassword"
- *    }
+ *          @example {json}:
+ *              {
+ *                  "email": "user@example.com",
+ *                  "password": "yourpassword"
+ *              }
  * 
- * @example {json} Example Success Response:
- *    {
- *      "RefreshToken": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...",
- *      "AccessToken": "eyJhbGciOiJIUzI1NiIsInR5cCI7gH8Trdua..."
- *    }
  * 
- * @example {json} Example Error Response (400):
- *    {
- *      "error": "Invalid request. Username and password are required."
- *    }
  * 
- * @example {json} Example Error Response (401):
- *    {
- *      "error": "Invalid password."
- *    }
+ * @responses
+ *      @SuccessResponse
+ *          @example {json} (200):
+ *          @description The user has inserted the right email and username and is given back with the tokens
+ *              {
+ *                 "RefreshToken": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...",
+ *                 "AccessToken": "eyJhbGciOiJIUzI1NiIsInR5cCI7gH8Trdua..."
+ *              }
  * 
- * @example {json} Example Error Response (404):
- *    {
- *      "error": "Account not registered"
- *    }
+ *      @ErrorResponse
+ *          @example {json} (400):
+ *          @description The user has inserted an invalis request: both Username and Password are required
+ *              {
+ *                  "error": "Invalid request. Username and password are required."
+ *              }
  * 
- * @example {json} Example Error Response (500):
- *    {
- *      "error": "An unexpected error occurred. Please try again later."
- *    }
+ *          @example {json} (401):
+ *          @description The user has inserted an invalid password
+ *              {
+ *                  "error": "Invalid password."
+ *              }
  * 
- * @example {curl} Example Usage:
- *    curl -X POST https://api.yourservice.com/api/login \
- *         -H "Content-Type: application/json" \
- *         -d '{"email": "user@example.com", "password": "yourpassword"}'
+ *          @example {json} (404):
+ *          @description The user has inserted an invalid email
+ *              {
+ *                  "error": "Account not registered."
+ *              }
+ * 
+ *          @example {json} (500):
+ *          @description The served has had some problems during login
+ *              {
+ *                  "error": "An error occured during login."
+ *              }
+ * 
+ * 
+ * 
+ * @usage
+ *      @example {curl} Example Usage:
+ *          curl -X POST https://api.yourservice.com/auth/login \
+ *               -H "Content-Type: application/json" \
+ *               -d '{"email": "user@example.com", "password": "yourpassword"}'
+ * 
+ * 
  */
 router.post(`/login`, async (req, res)=>{
     const email = req.body.email;
     const password = req.body.password;
-    let hased = await bcrypt.hash(password, 10)
-    console.log(`${hased}`)
+
     if (!email || !password){
         return res.status(400).json({error: `Invalid request. Username and password are required.`}); //Bad Request
     }
@@ -119,22 +134,47 @@ router.post(`/login`, async (req, res)=>{
 
     }catch(err){
         console.log(`An error occoured during authentication: ${err}`)
-        res.status(500)
+        res.status(500).json(`An error occured during login`)
     }
 })
 
 
 
 /**
- * POST /auth/logout
+ * /auth/logout
  * 
- * @description This endpoint logs out a user
+ * @description This endpoint logs out the user
  * 
  * @method DELETE
  * 
  * @endpoint_url https://api.yourservice.com/auth/logout
  * 
- * There is no example since the informatino needed from the logout are taken from the cookies
+ * 
+ * 
+ * @request
+ *      @Request_Header 
+ * 
+ *      @Request_Body The request body is empty since the AccessToken is passed through cookies
+ * 
+ * 
+ * 
+ * @responses
+ *      @SuccessResponse
+ *          @example (204)
+ *          @description The user has sucesfully logged out
+ * 
+ *      @ErrorResponse
+ *          @example {json} (500)
+ *              {
+ *                  "error": "An error occoured during logout"
+ *              }
+ * 
+ * 
+ * 
+ * @usage
+ *      @example {curl} Example Usage:
+ *          curl -X DELETE https://api.yourservice.com/auth/LOGOUT
+ * 
  */
 router.delete('/logout', authenticateToken, async (req, res) => {
     try{
@@ -145,13 +185,13 @@ router.delete('/logout', authenticateToken, async (req, res) => {
         res.clearCookie('accessToken');
         res.clearCookie('refreshToken');
     
-        res.status(204).json(session._id)
+        res.status(204)
 
         console.log(`The user -${req.user.username}- has succesfully logged out!`)
 
     }catch(err){
         console.log(`An error occoured during logout: ${err}`)
-        res.status(500)
+        res.status(500).json({error: `An error occured during logout`})
     }
     
 })
@@ -159,28 +199,59 @@ router.delete('/logout', authenticateToken, async (req, res) => {
 
 
 /**
- * POST /auth/posts
+ * /auth/posts
+ * 
+ * This endpoint will be updated when the user stories about asking for an object will be implemented. For now it is only a simple example
  * 
  * @description This endpoint authenticates a user and returns a content requested from the platfotm
  *  
  * @method GET
  * 
- * @endpoint_url https://api.yourservice.com/auth/posts
- 
- * The endpoint will be updated when the user stories about asking for an object will be implemented
+ * @endpoint_url https://api.yourservice.com/auth/post
+ * 
+ * 
+ * 
+ * @request
+ *      @Request_Header 
+ * 
+ *      @Request_Body The request body is empty since the AccessToken is passed through cookies
+ * 
+ * 
+ * 
+ * @responses
+ *      @SuccessResponse
+ *          @example {json} (200)
+ *          @description The data is returned from the db
+ *              {
+ *                  "data": "example_data"
+ *              }
+ * 
+ *      @ErrorResponse
+ *          @example {json} (500)
+ *          @description An error occured during requesting data from the website
+ *              {
+ *                  "error": "An error occured during requesting services to the db"
+ *              }
+ * 
+ * 
+ * 
+ * @usage
+ *      @example {curl} Example Usage:
+ *          curl -X GET https://api.yourservice.com/auth/posts
+ * 
  */
 router.get('/posts', authenticateToken, async (req, res) =>{
     try{
         //Search what the User wants
         const user = await User.findOne( {email: req.user.email} );
     
-        res.json(user.username)
+        res.status(200).json({data: user.username})
 
         console.log(`The user -${req.user.username}- has succesfully received data from the web-site!`)
 
     }catch(err){
         console.log(`An error occoured during requesting data: ${err}`)
-        res.status(500)
+        res.status(500).json(`An error occured during requesting services to the db`)
     }
     
 })
@@ -188,16 +259,43 @@ router.get('/posts', authenticateToken, async (req, res) =>{
 
 
 /**
- * POST /auth/token
+ * /auth/token
  * 
- * @description This endpoint generates a new accessToken for the user
+ * @description This endpoint generates a new AccessToken for the user
  * 
  * @method POST
  * 
  * @endpoint_url https://api.yourservice.com/auth/token
  * 
+ * @request
  * 
- * There is no example since the argumenti passing is done through cookies.
+ * @response
+ *      @SuccessResponse
+ *          @example {json} (200):
+ *          @description The Token is sucessfully generated and given back in the response
+ *              {
+ *                  "AccessToken": "eyJhbGciOiJIUzI1NiIsInR5cCI7gH8Trdua..."
+ *              }
+ * 
+ *      @ErrorResponse
+ *          @example (403)
+ *          @description You do not have the permission to access this part of the website (the Token is invalid)
+ * 
+ *          @example (401)
+ *          @description The User does not have the AccessToken
+ * 
+ *          @example {json} (500):
+ *          @description An error occured while crating a new token.
+ *              {
+ *                  "error": "An error occoured during requesting new token"
+ *              }
+ * 
+ * 
+ * 
+ * @usage
+ *      @example {curl} Example Usage:
+ *          curl -X POST https://api.yourservice.com/auth/token
+ * 
  */
 router.post('/token', async (req, res)=>{
     try{
@@ -224,20 +322,26 @@ router.post('/token', async (req, res)=>{
     
             res.cookie('accessToken', accessToken, { httpOnly: true, secure: true, sameSite: 'Strict' });
         
-            res.json({ accessToken: accessToken})
+            res.status(200).json({ accessToken: accessToken})
 
             console.log(`The user -${user.username}- has succesfully received a new token!`)
         })
     }catch(err){
         console.log(`An error occoured during requesting new token: ${err}`)
-        res.status(500)
+        res.status(500).json(`An error occured while creating a new token.`)
     }
     
 })
 
 
 
-/**MIDDLEWARE TO AUTHENTICATE THE TOKEN*/
+/**
+ * @description This middleware autheticates the token.
+ *
+ * @param {object} req The request object contais the cookies and so the AccessToken.
+ * @param {object} res The respons object contains the status that will be returned.
+ * @param {function} next The next function contains the next middleware function to call  
+*/
 function authenticateToken(req, res, next) {
     try{
         //take the access Token from the cookies if exists
@@ -258,7 +362,7 @@ function authenticateToken(req, res, next) {
 
     }catch(err){
         console.log(`An error occoured during token authentication: ${err}`)
-        res.status(500)
+        res.status(500).json(`An error occured during Token Authetication`)
     }
     
 }
