44fd6353207c006b04abae12024349fe96575fda
Kirgnition <riccardobuah@gmail.com>
2024-06-19 12:06:31 +0200

    removed deprecated options

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 8080;
  * Configure mongoose
  */
 mongoose.Promise = global.Promise;
-app.locals.db = mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+app.locals.db = mongoose.connect(process.env.MONGO_URI)
 .then ( () => {
     
     //console.log("Connected to Database");
