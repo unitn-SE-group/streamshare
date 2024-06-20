33669e50448d2eae9dd1fe6291d4fa84cb881d34
pedwoo <pedale.velocedp@gmail.com>
2024-06-20 14:08:53 +0200

    Merged file

--- app/app.js
+++ app/app.js
@@ -1,6 +1,7 @@
 import express from 'express'
-import login from './authentication.js'
+// import login from './authentication.js'
 import registration from './register.js'
+import oauth from './oauth.js'
 import { connect } from 'mongoose'
 import cors from 'cors'
 import dotenv from 'dotenv'
@@ -30,7 +31,8 @@ connect(process.env.MONGO_URI)
 app.use(express.json())
 app.use(cors())
 app.use('/auth', registration)
-app.use('/auth', login)
+// app.use('/auth', login)
+app.use('/oauth', oauth)
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 
 app.listen(3000, () => {
