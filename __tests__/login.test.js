b3cf89c4d1ee26a09a76e17c3ce4b3a394096be9
Gabbo717 <gabriele.bazzanellabauer@gmail.com>
2024-06-21 13:38:36 +0200

    added the two remaining tests and modifies one already present to fit the test case described in the google doc

--- __tests__/login.test.js
+++ __tests__/login.test.js
@@ -5,9 +5,9 @@ import app from '../app/app.js'
 
 describe('POST /auth/login', () => {
   /** 
-   * Send a POST request to the API login endpoint with an object containing the 
-   * correct email of an account already present in the mongoDB.
-   * This one provides the API the correct password.
+   * Send a POST request to the API login endpoint with an object email and password
+   * needed for the login
+   * This one provides the API the correct password and email
   */
   it('should respond with a 200 status and an object containing access and refresh tokens for existing user', async () => {
     const loginData = {
@@ -25,12 +25,34 @@ describe('POST /auth/login', () => {
 
   //This one provides the API the incorrect password.
 
-  it('should respond with a 404 status for wrong password or non-existing user', async () => {
+  it('should respond with a 401 status for wrong password', async () => {
     const wrongLoginData = {
-      email: 'wrongemailemail@email.com',
-      password: 'conigliotriste'
+      email: 'daniele.pedrolli@studenti.unitn.it',
+      password: 'notciaociao'
     }
     const res = await request(app).post('/auth/login').send(wrongLoginData)
+    expect(res.status).toBe(401)
+  })
+
+  //This one provides the API the incorrect amount of informations.
+
+  it('should respond with a 400 status for not providing all informations necessary', async () =>{
+    const missingLoginData = {
+      email : 'daniele.pedrolli@studenti.unitn.it'
+    }
+    const res = await request(app).post('/auth/login').send(missingLoginData)
+    expect(res.status).toBe(400)
+  })
+
+  //This one provides the API an email not present in the database.
+
+  it('should respond with a 404 status due to non-existent account', async () => {
+    const nonExistentEmailData = {
+      email : 'notdaniele.pedrolli@studenti.unitn.it',
+      password : 'ciaociao'
+    }
+    const res = await request(app).post('/auth/login').send(nonExistentEmailData)
     expect(res.status).toBe(404)
   })
+    
 })
