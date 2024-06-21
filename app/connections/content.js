ee588256f6e5cd6385b4bc617525146874740ba8
Kirgnition <riccardobuah@gmail.com>
2024-06-21 22:05:40 +0200

    fixed wrong variable being referenced

--- app/connections/content.js
+++ app/connections/content.js
@@ -7,14 +7,14 @@ dotenv.config();
 let content_connection;
 if (process.env.NODE_ENV !== 'test') {
     content_connection = mongoose.createConnection(process.env.MONGO_CONTENT_URI);
-    accounts_connection.on('error', console.error.bind(console, 'MongoDB content database connection error:'));
-    accounts_connection.once('open', function() {
+    content_connection.on('error', console.error.bind(console, 'MongoDB content database connection error:'));
+    content_connection.once('open', function() {
       console.log("Connected to MongoDB content database!");
     });
 } else {
     content_connection = mongoose.createConnection(process.env.MONGO_TEST_URI);
-    accounts_connection.on('error', console.error.bind(console, 'MongoDB test database connection error:'));
-    accounts_connection.once('open', function() {
+    content_connection.on('error', console.error.bind(console, 'MongoDB test database connection error:'));
+    content_connection.once('open', function() {
       console.log("Connected to MongoDB test database!");
     });
 }
