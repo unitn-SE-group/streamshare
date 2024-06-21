54dd8da894b356ee470802f9e7c9d0cb4b312cea
Kirgnition <riccardobuah@gmail.com>
2024-06-21 23:01:20 +0200

    refactored jest.setup.js to use the already existing connection

--- __tests__/jest.setup.js
+++ __tests__/jest.setup.js
@@ -1,23 +1,16 @@
-import mongoose from 'mongoose';
 import {User, Session} from '../app/connections/accounts.js';
 
-let mongoServer;
-
 beforeAll(async () => {
-    
 });
 
 afterAll(async () => {
     // drop the database
-    await mongoose.connect(process.env.MONGO_TEST_URI);
-    await mongoose.connection.dropDatabase();
-    await mongoose.connection.close();
+    User.collection.drop();
+    Session.collection.drop();
 });
 
 beforeEach(async () => {
-    await mongoose.connect(process.env.MONGO_TEST_URI);
 });
 
 afterEach(async () => {
-    await mongoose.connection.close();
 });
