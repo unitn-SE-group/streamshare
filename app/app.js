dbc3beb19e2498208cf4591fdc678c157baef63e
Davide <davidefacchinelli2003@gmail.com>
2024-06-24 12:23:13 +0200

    first version of working content retrieving

--- app/app.js
+++ app/app.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import login from './authentication.js'
 import registration from './register.js'
+import content from './view-content.js'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import swaggerJsdoc from 'swagger-jsdoc'
@@ -26,6 +27,7 @@ app.use(express.json())
 app.use(cors())
 app.use('/auth', registration)
 app.use('/auth', login)
+app.use('/content', content)
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 
 export default app
--- app/view-content.js
+++ app/view-content.js
@@ -1,21 +1,34 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const Grid = require('gridfs-stream');
-const { default: content_connection } = require('./connections/content');
+import { google } from 'googleapis';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+import express from 'express'
+import cookieParser from 'cookie-parser';
+import {Session} from './connections/accounts.js'
+import  content_connection from './connections/content.js';
+
+dotenv.config()
+const router = express.Router()
+
+router.use(cookieParser())
+
+const oauth2Client = new google.auth.OAuth2(
+  process.env.GOOGLE_CLIENT_ID,
+  process.env.GOOGLE_CLIENT_SECRET
+)
 
 /**
  * @openapi: 3.0.0
- * /auth/posts:
+ * /content:
  *   get:
- *     summary: Retrieve content from the platform
- *     description: This endpoint authenticates a user and returns content requested from the platform. This is a placeholder example and will be updated when user stories about requesting objects are implemented.
+ *     summary: Retrieve the catalog of films and video
+ *     description: This endpoint authenticates a user and returns the catalog present on the platform.
  *     responses:
  *       '200':
- *         description: The data is returned from the database.
+ *         description: The catalog is returned from the database.
  *         content:
  *           application/json:
  *             example:
- *               data: "example_data"
+ *               catalog: ["example_video_1.mp4" , "example_video_2.mp4"]
  *       '500':
  *         description: An error occurred during requesting data from the website.
  *         content:
@@ -28,31 +41,74 @@ const { default: content_connection } = require('./connections/content');
  *         value: |
  *           curl -X GET https://api.yourservice.com/auth/posts
  */
-router.get('/content', authenticateToken, async (req, res) => {
+router.get('', authenticateToken, async (req, res) => {
+  //Retriving the Content from the database
     try {
-      //Retriving the Content from the database
-      
-      /*FIRST OPTION
-        const catalog = await Content.find({});*/
 
+      const filesCollection = content_connection.collection(`upload.files`);
 
-      /*SECOND OPTION
-      const files = await content_connection.files.find().toArray();
-        if (!files || files.length === 0) {
-            return res.status(404).json({ message: 'No files found' });
-        }
+      const files = await filesCollection.find().toArray();
 
-        // Extract filenames
-        const filmNames = files.map(file => file.filename);
-        res.json(filmNames);
+      if (!files || files.length === 0) {
+          return res.status(404).json({ message: 'No files found' });
+      }
 
-      console.log(`The catalog is: ${catalog}`);*/
+      // Extract filenames
+      const filmNames = files.map(file => file.filename);
+
+      console.log(`The catalog is: ${filmNames}`);
 
       console.log(`The user -${req.user.username}- has succesfully received the catalog from the web-site!`)
   
-      return res.status(200).json({ catalog: catalog })
+      return res.status(200).json({ catalog: filmNames })
     } catch (err) {
       console.log(`An error occoured during requesting data: ${err}`)
       return res.status(500).json({ error: `An error occured during requesting services to the db` })
     }
   })
+
+async function authenticateToken(req, res, next) {
+  try {
+    //take the access Token from the cookies if exists
+    const token = req.cookies.accessToken
+
+    if (!token) {
+      return res.sendStatus(401)
+    }
+
+    // retrieve user type from db
+    const session = await Session.findOne({ accessToken: token }).populate('user_id')
+    const userType = session.user_id.userType
+
+    // authenticate based on user type
+    if (userType === 'google') {
+      oauth2Client
+        .verifyIdToken({
+          idToken: req.body.id_token,
+          audience: process.env.GOOGLE_CLIENT_ID
+        })
+        .then((ticket) => {
+          const payload = ticket.getPayload()
+          const userid = payload['sub']
+          console.log('userid', userid)
+          next()
+        })
+        .catch(console.error)
+      next()
+    } else {
+      //check whether the token is correct
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err) {
+          return res.sendStatus(403)
+        }
+        req.user = user
+        next()
+      })
+    }
+  } catch (err) {
+    console.log(`An error occoured during token authentication: ${err}`)
+    return res.status(500).json({ error: `An error occured during Token Authetication` })
+  }
+}
+
+export default router
