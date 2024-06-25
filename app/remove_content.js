cacbbeaec2eb0df134a8d4f441eb1432a492088f
Gabbo717 <gabriele.bazzanellabauer@gmail.com>
2024-06-25 15:06:04 +0200

    Added documentation in swagger for the API endpoint of the delete request

--- app/remove_content.js
+++ app/remove_content.js
@@ -7,6 +7,40 @@ import { authenticateToken } from './authentication.js';
 dotenv.config();
 const router = Router();
 
+/**
+ * @openapi
+ * /content/:contentId:
+ *  delete:
+ *    summary: delete a content from the database
+ *    description: Recieves the mongoDB id and removes the content with the corrisponding id in the database (if present)
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        properties:
+ *          params:
+ *            type: string
+ *            description: the id of the content the requester wants to remove
+ *    responses:
+ *      204:
+ *        description: Content deleted successfully.
+ *      400:
+ *        description: Invalid ID format.
+ *      500:
+ *        description: Error deleting file.
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                message:
+ *                  type: string
+ *                  description: An error message.
+ *            
+ * 
+ */
+
+
+
 // Establish MongoDB connection using Mongoose
 mongoose.connect(process.env.MONGO_CONTENT_URI);
 const conn = mongoose.connection;
@@ -20,21 +54,19 @@ conn.once('open', () => {
 });
 
 router.delete('/:contentId', [authenticateToken('admin', 'creator')], async (req, res) => {
-  console.log('Received DELETE request');
   const { contentId } = req.params;
-  console.log('Content ID:', contentId);
 
   if (!mongoose.Types.ObjectId.isValid(contentId)) {
-    return res.status(400).json({ error: 'Invalid ID format' });
+    return res.status(400).json({ error: 'Invalid ID format.' });
   }
 
   try {
     // Delete file from GridFS using the file's ObjectId
     await gfs.delete(new mongoose.Types.ObjectId(contentId));
-    res.status(200).json({ message: 'File deleted successfully' });
+    res.status(204).json({ message: 'File deleted successfully.' });
   } catch (error) {
     console.error('Error deleting file:', error);
-    return res.status(500).json({ error: 'Error deleting file', details: error.message });
+    return res.status(500).json({ error: 'Error deleting file.', details: error.message });
   }
 });
 
