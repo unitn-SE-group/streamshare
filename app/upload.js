24137dfb8e5ff04884cea61522731e82768a6120
Kirgnition <riccardobuah@gmail.com>
2024-06-25 15:13:15 +0200

    written documentation

--- app/upload.js
+++ app/upload.js
@@ -20,6 +20,91 @@ const storage = new GridFsStorage({
 
 const upload  = multer({storage})
 
+
+/**
+ * @openapi
+ * /content:
+ *   post:
+ *     summary: Upload a video.
+ *     description: Receives a video file and uploads it to the database.
+ *     tags:
+ *      - Content
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         multipart/form-data:
+ *           schema:
+ *             type: object
+ *             properties:
+ *              file:
+ *                  type: string
+ *                  format: binary
+ *                  description: The video file to upload
+ *
+ *     responses:
+ *       '201':
+ *         description: File uploaded successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 file:
+ *                   type: object
+ *                   properties:
+ *                     fieldname:
+ *                       type: string
+ *                     originalname:
+ *                       type: string
+ *                     encoding:
+ *                       type: string
+ *                     mimetype:
+ *                       type: string
+ *                     id:
+ *                       type: string
+ *                     filename:
+ *                       type: string
+ *                     metadata:
+ *                       type: object
+ *                     bucketName:
+ *                       type: string  
+ *                     chunkSize:
+ *                       type: integer
+ *                     size:
+ *                       type: integer
+ *                     uploadDate:
+ *                       type: string
+ *                     contentType:
+ *                       type: string
+ *       '400':
+ *         description: No file uploaded
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: string
+ *               example: 'No file uploaded.'
+ *       '401':
+ *         description: Unauthorized. Access token is missing or invalid.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: string
+ *               example: 'Unauthorized'
+ *       '403':
+ *         description: Forbidden. User does not have the necessary permissions.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: string
+ *               example: 'Forbidden'
+ *       '500':
+ *         description: Internal Server Error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: string
+ *               example: 'Internal Server Error'
+ * */
 router.post('/', [ authenticateToken('admin','creator'), upload.single('file') ], async (req, res) => {
     try {
         if (!req.file) {
