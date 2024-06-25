95aec048b22c0b28b8ace9da890875de14e226a7
Gabbo717 <gabriele.bazzanellabauer@gmail.com>
2024-06-25 11:34:56 +0200

    added token autentication middleware

--- app/upload.js
+++ app/upload.js
@@ -2,6 +2,7 @@ import { Router } from 'express'
 import multer from 'multer'
 import dotenv from 'dotenv'
 import { GridFsStorage } from 'multer-gridfs-storage'
+import { authenticateToken } from './authentication.js'
 
 dotenv.config();
 const router = Router();
@@ -19,14 +20,16 @@ const storage = new GridFsStorage({
 
 const upload  = multer({storage})
 
-router.post('/', upload.single('file'), async (req, res) => {
-    // TODO check user permission
-
-    if(!req.file) {
-        return send.status(400).send('No file uploaded.');
+router.post('/', [ authenticateToken('admin','creator'), upload.single('file') ], async (req, res) => {
+    try {
+        if (!req.file) {
+            return res.status(400).send('No file uploaded.');
+        }
+        res.status(201).send({
+            file: req.file,
+        });
+    } catch (error) {
+        res.status(500).send('Internal Server Error');
     }
-    res.status(201).send({
-        file: req.file
-    });
 })
 export default router;
